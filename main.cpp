9601b96ed41b2ea2c3fcb3d0dda235ffe2b985d3
Saleh Ghobari <kreanal355@outlook.com>
2025-03-18 23:30:33 +0300

    finished project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include "raylib.h"
 
+
+
+
+enum gameState
+{
+    MENU,
+    GAME,
+    EXIT
+};
+
+
+
+
 int main()
 {
     std::srand(static_cast<unsigned int>(std::time(0)));
@@ -33,7 +46,13 @@ int main()
     // Circle stuff
     Vector2 ballCenter = { screenWidth / 2.0, screenHeight / 2.0};
     Vector2 ballReset = { screenWidth / 2.0, screenHeight / 2.0 };
+
+    // Menu Rectangles
+    Rectangle playButton = { screenWidth / 2.0 - 100, screenHeight / 2.0 - 25, 200, 50 };
+    Rectangle exitButton = { screenWidth / 2.0 - 100, screenHeight / 2.0 + 100, 200, 50 };
     
+    gameState CurrentState = MENU;
+
     
     
 
@@ -46,121 +65,149 @@ int main()
     Sound paddleHit = LoadSound("../Assets/paddleHit.mp3");
     Sound pointWon = LoadSound("../Assets/Point.mp3");
     // Main game loop
-    while (!exit)    // Detect window close button or ESC key
+    while (CurrentState != EXIT)    // Detect window close button or ESC key
     {
+
         float deltaTime = GetFrameTime();
+        if (CurrentState == MENU)
+        {
+            Vector2 mousePos = GetMousePosition();
+            if (CheckCollisionPointRec(mousePos, playButton) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            {
+                CurrentState = GAME;
+            }
+            if (CheckCollisionPointRec(mousePos, exitButton) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            {
+                CurrentState = EXIT;
+            }
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("PONG GAME", screenWidth / 2 - 80, 150, 60, PURPLE);
+            DrawRectangleRec(playButton, LIGHTGRAY);
+            DrawRectangleRec(exitButton, LIGHTGRAY);
+            DrawText("PLAY", playButton.x + 70, playButton.y, 40, BLACK);
+            DrawText("EXIT", exitButton.x + 70, exitButton.y, 40, BLACK);
+            EndDrawing();
+        }
         // Update
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
-        if (WindowShouldClose() || IsKeyPressed(KEY_ESCAPE))
+        else if (CurrentState == GAME)
         {
-            exitRequest = true;
-        }
-        if (exitRequest)
-        {
-            if (IsKeyPressed(KEY_Y))
+            if (WindowShouldClose() || IsKeyPressed(KEY_ESCAPE))
             {
-                exit = true;
+                exitRequest = true;
+            }
+            if (exitRequest)
+            {
+                if (IsKeyPressed(KEY_Y))
+                {
+                    exit = true;
+                    CurrentState = EXIT;
+                }
+
+                else if (IsKeyPressed(KEY_N))
+                {
+                    exitRequest = false;
+                    CurrentState = GAME;
+                }
+            }
+            if (!resetBall)
+            {
+                ballCenter.x += ballVx * deltaTime;
+                ballCenter.y += ballVy * deltaTime;
             }
 
-            else if (IsKeyPressed(KEY_N))
+            if (!resetBall && CheckCollisionCircleRec(ballCenter, ballRadius, rightBorder))
             {
-                exitRequest = false;
+                leftPlayerScore += 1;
+                PlaySound(pointWon);
+                resetBall = true;
             }
-        }
-        if (!resetBall)
-        {
-            ballCenter.x += ballVx * deltaTime;
-            ballCenter.y += ballVy * deltaTime;
-        }
+            if (!resetBall && CheckCollisionCircleRec(ballCenter, ballRadius, leftBorder))
+            {
+                rightPlayerScore += 1;
+                PlaySound(pointWon);
+                resetBall = true;
+            }
+            if (resetBall)
+            {
+                ballCenter = ballReset;
+                ballVx = ballSpeedMultiplier * (rand() % 2 == 0 ? -1 : 1);
+                ballVy = ballSpeedMultiplier * (rand() % 2 == 0 ? -1 : 1);
+                resetBall = false;
+            }
 
-        if (!resetBall && CheckCollisionCircleRec(ballCenter, ballRadius, rightBorder))
-        {
-            leftPlayerScore += 1;
-            PlaySound(pointWon);
-            resetBall = true;
-        }
-        if (!resetBall && CheckCollisionCircleRec(ballCenter, ballRadius, leftBorder))
-        {
-            rightPlayerScore += 1;
-            PlaySound(pointWon);
-            resetBall = true;
-        }
-        if (resetBall)
-        {
-            ballCenter = ballReset;
-            ballVx = ballSpeedMultiplier * (rand() % 2 == 0 ? -1 : 1);
-            ballVy = ballSpeedMultiplier * (rand() % 2 == 0 ? -1 : 1);
-            resetBall = false;
-        }
+            // CheckCollisionCircleRec(Vector2 center, float radius, Rectangle rec); 
+            if (CheckCollisionCircleRec(ballCenter, ballRadius, rightPlayer) || CheckCollisionCircleRec(ballCenter, ballRadius, leftPlayer))
+            {
+                ballVx *= -1;
+                PlaySound(paddleHit);
+            }
+            if (CheckCollisionCircleRec(ballCenter, ballRadius, topBorder) || CheckCollisionCircleRec(ballCenter, ballRadius, bottomBorder))
+            {
+                ballVy *= -1;
+            }
 
-        // CheckCollisionCircleRec(Vector2 center, float radius, Rectangle rec); 
-        if (CheckCollisionCircleRec(ballCenter, ballRadius, rightPlayer) || CheckCollisionCircleRec(ballCenter, ballRadius, leftPlayer))
-        {
-            ballVx *= -1;
-            PlaySound(paddleHit);
+            if (IsKeyDown(KEY_W) && !CheckCollisionRecs(leftPlayer, topBorder))
+            {
+                leftPlayer.y -= playerSpeed * deltaTime;
+            }
+            if (IsKeyDown(KEY_S) && !CheckCollisionRecs(leftPlayer, bottomBorder))
+            {
+                leftPlayer.y += playerSpeed * deltaTime;
+            }
+            if (IsKeyDown(KEY_UP) && !CheckCollisionRecs(rightPlayer, topBorder))
+            {
+                rightPlayer.y -= playerSpeed * deltaTime;
+            }
+            if (IsKeyDown(KEY_DOWN) && !CheckCollisionRecs(rightPlayer, bottomBorder))
+            {
+                rightPlayer.y += playerSpeed * deltaTime;
+            }
+            //----------------------------------------------------------------------------------
+
+            // Draw
+            //----------------------------------------------------------------------------------
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawFPS(0, 0);
+            // Draw Seperating line
+
+            DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, PURPLE);
+
+            // Draw Player Paddles
+            DrawRectanglePro(leftPlayer, { 0, 0 }, 0, PURPLE);
+            DrawRectanglePro(rightPlayer, { 0, 0 }, 0, PURPLE);
+
+            // Draw Border Rectangles
+            DrawRectanglePro(topBorder, { 0, 0 }, 0, PURPLE);
+            DrawRectanglePro(bottomBorder, { 0, 0 }, 0, PURPLE);
+            DrawRectanglePro(rightBorder, { 0, 0 }, 0, PURPLE);
+            DrawRectanglePro(leftBorder, { 0, 0 }, 0, PURPLE);
+
+            // Draw Ball
+            DrawCircleV(ballCenter, ballRadius, ORANGE);
+
+            // Draw Scores
+            DrawText(TextFormat("%d", rightPlayerScore), 1440, 100, 50, PURPLE);
+            DrawText(TextFormat("%d", leftPlayerScore), 480, 100, 50, PURPLE);
+
+            // Draw exit confirmation
+            if (exitRequest)
+            {
+                //DrawText(const char *text, int posX, int posY, int fontSize, Color color); 
+                DrawRectangle(screenWidth / 2 - textMeasurement.x / 2, screenHeight / 2.0 - textMeasurement.y / 2, textMeasurement.x + 186, textMeasurement.y, WHITE);
+                DrawText("Are you sure you want to exit ? [Y/N]", screenWidth / 2 - textMeasurement.x / 2, screenHeight / 2.0 - textMeasurement.y / 2, 30, PURPLE);
+                std::cout << textMeasurement.x << "\n";
+            }
+
+            EndDrawing();
         }
-        if (CheckCollisionCircleRec(ballCenter, ballRadius, topBorder) || CheckCollisionCircleRec(ballCenter, ballRadius, bottomBorder))
-        {
-            ballVy *= -1;
-        }
-
-        if (IsKeyDown(KEY_W) && !CheckCollisionRecs(leftPlayer, topBorder))
-        {
-            leftPlayer.y -= playerSpeed * deltaTime;
-        }
-        if (IsKeyDown(KEY_S) && !CheckCollisionRecs(leftPlayer, bottomBorder))
-        {
-            leftPlayer.y += playerSpeed * deltaTime;
-        }
-        if (IsKeyDown(KEY_UP) && !CheckCollisionRecs(rightPlayer, topBorder))
-        {
-            rightPlayer.y -= playerSpeed * deltaTime;
-        }
-        if (IsKeyDown(KEY_DOWN) && !CheckCollisionRecs(rightPlayer, bottomBorder))
-        {
-            rightPlayer.y += playerSpeed * deltaTime;
-        }
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-        ClearBackground(BLACK);
-        DrawFPS(0, 0);
-        // Draw Seperating line
-
-        DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, PURPLE);
-
-        // Draw Player Paddles
-        DrawRectanglePro(leftPlayer, {0, 0}, 0, PURPLE);
-        DrawRectanglePro(rightPlayer, { 0, 0 }, 0, PURPLE);
-
-        // Draw Border Rectangles
-        DrawRectanglePro(topBorder, { 0, 0 }, 0, PURPLE);
-        DrawRectanglePro(bottomBorder, { 0, 0 }, 0, PURPLE);
-        DrawRectanglePro(rightBorder, { 0, 0 }, 0, PURPLE);
-        DrawRectanglePro(leftBorder, { 0, 0 }, 0, PURPLE);
-
-        // Draw Ball
-        DrawCircleV(ballCenter, ballRadius, ORANGE);
-
-        // Draw Scores
-        DrawText(TextFormat("%d", rightPlayerScore), 1440, 100, 50, PURPLE);
-        DrawText(TextFormat("%d", leftPlayerScore), 480, 100, 50, PURPLE);
-
-        // Draw exit confirmation
-        if (exitRequest)
-        {
-            //DrawText(const char *text, int posX, int posY, int fontSize, Color color); 
-            DrawRectangle(screenWidth / 2 - textMeasurement.x / 2 , screenHeight / 2.0 - textMeasurement.y / 2, textMeasurement.x, textMeasurement.y, WHITE);
-            DrawText("Are you sure you want to exit ?", (screenWidth / 2 - textMeasurement.x / 2) + 10, (screenHeight / 2.0 - textMeasurement.y / 2) + 10, 30, PURPLE);
-        }
-        
-        EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
+    
     // De-Initialization
     //--------------------------------------------------------------------------------------
     CloseWindow();        // Close window and OpenGL context
